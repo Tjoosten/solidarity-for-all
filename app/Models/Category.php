edba78bef3d8b4f8bf0ea12e2d8db44197813166
Tjoosten <Topairy@gmail.com>
2019-10-13 01:07:25 +0200

    Implement creatorable trait

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Relations\Creatorable;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Category
+ *
+ * @package App\Models
+ */
 class Category extends Model
 {
-    //
+    use Creatorable;
+
+    /**
+     * Protected fields for the internal mass-assignment system.
+     *
+     * @var array
+     */
+    protected $guarded = [];
 }
