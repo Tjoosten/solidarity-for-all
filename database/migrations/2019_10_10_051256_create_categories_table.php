36fa2950907fbc55338ea1efa39a88278a527bc6
Tjoosten <Topairy@gmail.com>
2019-10-13 01:10:54 +0200

    Implement category migration

--- database/migrations/2019_10_10_051256_create_categories_table.php
+++ database/migrations/2019_10_10_051256_create_categories_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Class CreateCategoriesTable
+ */
 class CreateCategoriesTable extends Migration
 {
     /**
@@ -11,11 +14,18 @@ class CreateCategoriesTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('categories', static function (Blueprint $table): void {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('creator_id')->index();
+            $table->string('name')->unique();
+            $table->text('info_or_description')->nullable();
             $table->timestamps();
+
+            // Foreign keys
+            $table->foreign('creator_id')->references('id')
+                ->on('users')->onDelete('set null');
         });
     }
 
@@ -24,7 +34,7 @@ class CreateCategoriesTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('categories');
     }
