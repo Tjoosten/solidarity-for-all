ca945eabec1fd3e837ddf84fc2a2d913c1489989
Tim Joosten <Tjoosten3@gmail.com>
2019-10-15 23:31:19 +0200

    WIP

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,31 @@
+@extends ('layouts.app', ['title' => ucfirst($user->name)])
+
+@section('content')
+    <div class="container-fluid py-3">
+        <div class="page-header">
+            <h1 class="page-title">Gebruikersbeheer</h1>
+            <div class="page-subtitle">Algemene informatie omtrent {{ ucfirst($user->name) }}</div>
+
+            <div class="page-options d-flex">
+                <a href="{{ route('users.index') }}" class="btn btn-orange">
+                    <i class="fe fe-list mr-2"></i> Overzicht
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid pb-3">
+        <div class="row">
+            <div class="col-3"> {{-- Sidebar --}}
+            </div> {{-- /// ENd sidebar Sidebar --}}
+
+            <div class="col-9"> {{-- Content --}}
+                <form action="" method="POST" class="card card-body shadow-sm border-0">
+
+                    @can ('update', $user) {{-- The user is an webmaster so he is permitted to change the account information --}}
+                    @endcan {{-- /// END authorization check --}}
+                </form>
+            </div> {{-- /// END content --}}
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,6 @@ Route::patch('/categorie/{category}', [CategoryController::class, 'update'])->na
 
 // User management routes
 Route::get('/gebruikers', [UsersControllers::class, 'index'])->name('users.index');
+Route::get('/gebruiker/{user}', [UsersControllers::class, 'show'])->name('users.show');
 Route::get('/nieuwe-gebruiker', [UsersControllers::class, 'create'])->name('users.create');
 Route::post('nieuwe-gebruiker', [UsersControllers::class, 'store'])->name('users.store');
