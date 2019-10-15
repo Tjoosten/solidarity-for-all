244d542ef1089738145994c2d6fa598be6b49afd
Tjoosten <Topairy@gmail.com>
2019-10-16 01:07:19 +0200

    implement functionality to mail the user

--- resources/views/users/_partials/sidenav.blade.php
+++ resources/views/users/_partials/sidenav.blade.php
@@ -2,4 +2,8 @@
     <a href="{{ route('users.show', $user) }}" class="list-group-item {{ active('users.show') }} list-group-item-action">
         <i class="fe fe-info text-muted mr-2"></i> Algemene informatie
     </a>
+
+    <a href="mailto:{{ $user->email }}" class="list-group-item list-group-item-action">
+        <i class="fe fe-mail text-muted mr-2"></i> Contacteer gebruiker
+    </a>
 </div>
